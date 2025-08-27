118734c61f3d0a8a62936f654477704134dfc913
Manvendra Upadhyay <manvendraupadhyay.cs@gmail.com>
2025-08-27 16:57:12 +0000

    Remove the dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={cn("font-sans antialiased", inter.variable, anton.variable, caveat.variable)}>
         {children}
         <Toaster />
